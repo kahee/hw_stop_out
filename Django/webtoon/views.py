e61c6810544c00bc7d093633d48b55c350d5bcaa
kahee <yuygh131@gmail.com>
2018-02-03 00:12:19 +0900

    confg/urls 와 webtoon/urls추가 그리고 view 에  webtoon_list 추가

--- Django/webtoon/views.py
+++ Django/webtoon/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from webtoon.models import Webtoon
+
+
+def webtoon_list(request):
+    webtoon = Webtoon.objects.all()
+    context = {
+        'webtoon': webtoon
+    }
+
+    return HttpResponse(webtoon)
