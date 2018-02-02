affba2b4d6000fb4359a45fcb63a9e5c9264aa06
kahee <yuygh131@gmail.com>
2018-02-03 00:39:30 +0900

    webtoon_detail.html 초안 완성, webtoon_list.html 에서 해당 제목을 누르면, pk에 따른 페이지로 넘어감.

--- Django/webtoon/urls.py
+++ Django/webtoon/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # localhost:8000/
     path('', views.webtoon_list, name='webtoon-list'),
+    # localhost:8000/1
+    path('<int:pk>/', views.webtoon_detail, name='webtoon-detail')
 ]
--- Django/webtoon/views.py
+++ Django/webtoon/views.py
@@ -6,9 +6,15 @@ from webtoon.models import Webtoon
 
 
 def webtoon_list(request):
+    # 저장된 웹툰 리스트를 보여줌
     webtoons = Webtoon.objects.all()
     context = {
         'webtoons': webtoons
     }
+    return render(request, 'webtoon/webtoon_list.html', context)
 
-    return render(request,'webtoon/webtoon_list.html',context)
+
+def webtoon_detail(request, pk):
+    episode = Webtoon.objects.get(pk=pk)
+
+    return HttpResponse(episode)
